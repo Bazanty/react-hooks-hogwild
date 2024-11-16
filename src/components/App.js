6493c872ecb1ad07a0ca698ea316d12624fde1da
bazanty <bazantyloffan@gmail.com>
2024-11-16 16:39:48 +0300

    project

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,71 @@
-import React from "react";
-import Nav from "./Nav";
+import React, { useState } from 'react';
+import Nav from './Nav';
+import hogs from '../porkers_data';
+import Card from './Cards';
 
-import hogs from "../porkers_data";
+import HogForm from './Form';
+import Sort from './Sort';
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+    const [option, setOption] = useState('none');
+    const [hogList, setHogList] = useState(hogs);
+    const [showGreased, setShowGreased] = useState(false);
+
+
+    const handleSortChange = (e) => {
+        setOption(e.target.value);
+    };
+
+    const handleGreasedChange = (e) => {
+        setShowGreased(e.target.checked);
+    };
+
+    const addHog = (newHog) => {
+        setHogList([...hogList, newHog]);
+    };
+
+    const filteredHogs = hogList.filter(hog => {
+        return showGreased ? hog.greased : true;
+    });
+
+    const sortedHogs = [...filteredHogs].sort((a, b) => {
+        if (option === 'name') {
+            return a.name.localeCompare(b.name);
+        } else if (option === 'weight') {
+            return b.weight - a.weight;
+        }
+        return 0;
+    });
+
+    return (
+        <div className="App">
+            <Nav />
+            <Sort option={option} onSortChange={handleSortChange} />
+            <div className="filter-options">
+                <label htmlFor="greased-filter">Show Greased Only</label>
+                <input
+                    type="checkbox"
+                    id="greased-filter"
+                    checked={showGreased}
+                    onChange={handleGreasedChange}
+                />
+            </div>
+            <div className="ui grid container">
+                {sortedHogs.map((hog) => (
+                    <Card
+                        key={hog.name}
+                        name={hog.name}
+                        specialty={hog.specialty}
+                        greased={hog.greased}
+                        weight={hog.weight}
+                        highest={hog['highest medal achieved']}
+                        image={hog.image}
+                    />
+                ))}
+            </div>
+            <HogForm addHog={addHog} />
+        </div>
+    );
 }
 
 export default App;
